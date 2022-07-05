2d848200916747a43379db8e5484b869503b30ab
Nithiya03 <nithiyasrirud03@gmail.com>
2022-07-05 15:15:47 +0530

    Nodejs

---
+++ queryString.js
@@ -0,0 +1,10 @@
+const queryString=require('querystring')
+let urlQuery="user=Nithiya&access=true&role=trainee&id=1234&practice=LAMP"
+let parsedObject = queryString.parse(urlQuery);
+console.log("Parsed Object:", parsedObject);
+  
+
+//Objects to URL query
+let urlObject = {user: "kavya",access: true,role: "trainee",id:'1234',practice:'LAMP'};
+let parsedQuery = queryString.stringify(urlObject);
+console.log("\nParsed Query:", parsedQuery);
